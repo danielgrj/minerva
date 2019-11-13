1e5066641986372ba13304cb2a86f2056e355a6b
Daniel Garcia <jdanielink@gmail.com>
2019-11-12 22:53:09 -0600

    Delete facebook and google routes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,17 +8,4 @@ router.post('/signup', createUser);
 router.post('/logout', logout);
 router.post('/edit', isLoggedIn, updateUser);
 
-router.get('/facebook', passport.authenticate('facebook', { scope: ['email'], display: 'popup' }));
-router.get('/facebook/callback', (req, res, next) =>
-  passport.authenticate('facebook' /*, finishSignup(req, res, next))(req, res, next*/)
-);
-
-router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'], display: 'popup' }));
-router.get(
-  '/google/callback',
-  passport.authenticate('google' /*, finishSignup(req, res, next))(req, res, next*/, {
-    successRedirect: 'http://localhost:3001/'
-  })
-);
-
 module.exports = router;
