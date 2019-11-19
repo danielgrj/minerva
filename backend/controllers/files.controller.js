9d0c32adebadca5117d6eccbf307b3a78c6fa73b
Daniel Garcia <jdanielink@gmail.com>
2019-11-18 18:24:36 -0600

    Delete unnecesary controller files

--- backend/controllers/files.controller.js
+++
@@ -1,4 +0,0 @@
-exports.uploadImage = (req, res) => {
-  if (req.file) return res.status(200).json({ msg: 'Ok', url: req.file.secure_url });
-  res.status(400).json({ msg: 'A file is required' });
-};
