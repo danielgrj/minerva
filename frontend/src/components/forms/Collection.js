2e4c2cb8d720fc37b0174cdac97a290c68e58145
Daniel Garcia <jdanielink@gmail.com>
2019-12-07 23:18:48 -0600

    Delete unnecesary comments

--- frontend/src/components/forms/Collection.js
+++ frontend/src/components/forms/Collection.js
@@ -1,16 +1,16 @@
-import React, { useEffect, useState, useContext } from 'react'
-import { CSSTransitionGroup } from 'react-transition-group'
-import { CollectionsContext } from '../../context/CollectionsContext'
-import { StylesContext } from '../../context/StylesContext'
+import React, { useEffect, useState, useContext } from 'react';
+import { CSSTransitionGroup } from 'react-transition-group';
+import { CollectionsContext } from '../../context/CollectionsContext';
+import { StylesContext } from '../../context/StylesContext';
 
-import './collection.css'
+import './collection.css';
 
 export default function Collection(props) {
-  const [ isVisible, setIsVisible ] = useState(false)
-  const [ collectionData, setCollectionData ] = useState({
+  const [isVisible, setIsVisible] = useState(false);
+  const [collectionData, setCollectionData] = useState({
     name: '',
     defaultStyle: ''
-  })
+  });
 
   const {
     createOneCollection,
@@ -18,59 +18,61 @@ export default function Collection(props) {
     getOneCollection,
     updateOneCollection,
     cleanCurrentCollection
-  } = useContext(CollectionsContext)
+  } = useContext(CollectionsContext);
 
   const { styles } = useContext(StylesContext);
 
   useEffect(() => {
-    setIsVisible(true)
+    setIsVisible(true);
 
-    document.body.classList.add('block-scroll')
+    document.body.classList.add('block-scroll');
 
     return () => {
-      document.body.classList.remove('block-scroll')
-    }
-  }, [])
+      document.body.classList.remove('block-scroll');
+    };
+  }, []);
 
   useEffect(() => {
     if ((!collectionData._id && props.match.params.id) || collectionData._id !== props.match.params.id) {
-      
-      getOneCollection(props.match.params.id)
+      getOneCollection(props.match.params.id);
       for (const key in currentCollection) {
-        if (currentCollection[key] === null) currentCollection[key] = ''
+        if (currentCollection[key] === null) currentCollection[key] = '';
       }
 
-      setCollectionData(currentCollection)
+      setCollectionData(currentCollection);
     }
-  }, [currentCollection, collectionData._id, getOneCollection, props.match.params.id])
+  }, [currentCollection, collectionData._id, getOneCollection, props.match.params.id]);
 
-  const handleInput = (e) => {
+  const handleInput = e => {
     setCollectionData({ ...collectionData, [e.target.name]: e.target.value });
-  }
+  };
 
-  const onSubmit = async (e) => {
-    e.preventDefault()
+  const onSubmit = async e => {
+    e.preventDefault();
     if (collectionData._id) {
-      updateOneCollection(collectionData._id, collectionData)
+      updateOneCollection(collectionData._id, collectionData);
     } else {
-      createOneCollection(collectionData)
+      createOneCollection(collectionData);
     }
 
-    cleanCurrentCollection()
-    setIsVisible(false)
+    cleanCurrentCollection();
+    setIsVisible(false);
     setTimeout(() => {
-      props.history.goBack()
-    }, 200)
-  }
+      props.history.goBack();
+    }, 200);
+  };
 
   return (
     <div className="route-container">
-      <div className="back-container" onClick={() => {
-        setIsVisible(false)
-        setTimeout(() => {
-          props.history.goBack()
-        }, 250)
-      }}></div>
+      <div
+        className="back-container"
+        onClick={() => {
+          setIsVisible(false);
+          setTimeout(() => {
+            props.history.goBack();
+          }, 250);
+        }}
+      ></div>
       <CSSTransitionGroup
         transitionName="quote"
         transitionAppearTimeout={600}
@@ -78,27 +80,27 @@ export default function Collection(props) {
         transitionLeaveTimeout={200}
         component="div"
       >
-        {isVisible ?
+        {isVisible ? (
           <div className="collection-container">
             <form className="collection" onSubmit={onSubmit}>
               <h2>{props.match.params.id ? 'Edit collection' : 'Add collection'}</h2>
               <label htmlFor="name">Name of the collection</label>
-              <input type="text" name="name" id="name" onChange={handleInput} value={collectionData.name}/>
+              <input type="text" name="name" id="name" onChange={handleInput} value={collectionData.name} />
               <label htmlFor="defaulStyle">Style</label>
-              <select name="defaultStyle" id="defaultStyle" onChange={handleInput}>
+              <option name="defaultStyle" id="defaultStyle" onChange={handleInput}>
                 {styles.map(({ _id, name }) => (
-                  <option key={_id} value={_id}>{name}</option>
+                  <option key={_id} value={_id}>
+                    {name}
+                  </option>
                 ))}
-              </select>
-              {/* <label htmlFor="quotes">Add quotes</label>
-              <input type="text" name="quotes" id="quotes" />
-              <label htmlFor="contributors">Add contributors</label>
-              <input type="text" name="contributors" id="contributors" /> */}
-              <input type="submit" value="Save collection"/>
+              </option>
+              <input type="submit" value="Save collection" />
             </form>
           </div>
-          : undefined}
+        ) : (
+          undefined
+        )}
       </CSSTransitionGroup>
     </div>
-  )
+  );
 }
